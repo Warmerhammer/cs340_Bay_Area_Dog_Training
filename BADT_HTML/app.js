324cf542cd66deb9df83645712b14d8a1f390103
Daniel Bracamontes <59780347+Dedzed101@users.noreply.github.com>
2022-07-17 12:32:15 -0700

    Updated app.js - HTML site is navigable

--- BADT_HTML/app.js
+++ BADT_HTML/app.js
@@ -1,55 +1,67 @@
 //Initialize modules
 var express = require("express");
-var handlebars = require("express-handlebars").create({ defaultLayout: 'main' });
+// var handlebars = require("express-handlebars").create({ defaultLayout: 'main' });
 var path = require("path");
 const app = express();
+// const router = express.Router();
+app.use(express.urlencoded());
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
 
 //Configures 'hbs' as the container for '.hbs' extension 
 //app.engine('hbs', exphbs({ extname: '.hbs' }))
-app.engine('handlebars', handlebars.engine)
-app.set('view engine', 'handlebars')
+// app.engine('handlebars', handlebars.engine)
+// app.set('view engine', 'handlebars')
 //app.use(express.static(path.join(__dirname + '/public')));
 app.set('port', 3000);              // Set a port number at the top so it's easy to change in the future
 
 /*
     ROUTES
 */
-app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
+app.get('/', function(req, res)                
     {
-        res.sendFile(__dirname + '/Customers.html');      // This function literally sends the string "The server is running!" to the computer
-    });                                         // requesting the web site.
+        res.sendFile(path.join(__dirname + '/Customers.html'));      
+    });                                         
 
 app.get('/Dogs', function(req, res)                
 {
-    res.sendFile(__dirname + '/Dogs.html');      
+    res.sendFile(path.join(__dirname + '/Dogs.html'));      
 });       
 
-app.get('/', function(req, res)                
+app.get('/Dog_has_Training_Session', function(req, res)                
 {
-    res.sendFile(__dirname + '/Customers.html');      
+    res.sendFile(path.join(__dirname + '/Dog_has_Training_Session.html'));      
 }); 
 
-app.get('/', function(req, res)                
+app.get('/Packages', function(req, res)                
 {
-    res.sendFile(__dirname + '/Customers.html');      
+    res.sendFile(path.join(__dirname + '/Packages.html'));      
 }); 
 
-app.get('/', function(req, res)                
+app.get('/Purchases', function(req, res)                
 {
-    res.sendFile(__dirname + '/Customers.html');      
+    res.sendFile(path.join(__dirname + '/Purchases.html'));      
 }); 
 
-app.get('/', function(req, res)                
+app.get('/Session_Types', function(req, res)                
 {
-    res.sendFile(__dirname + '/Customers.html');      
+    res.sendFile(path.join(__dirname + '/Session_Types.html'));      
 }); 
 
-app.get('/', function(req, res)                
+app.get('/Trainers', function(req, res)                
 {
-    res.sendFile(__dirname + '/Customers.html');      
+    res.sendFile(path.join(__dirname + '/Trainers.html'));      
+}); 
+
+app.get('/Trainer_has_Training_Session', function(req, res)                
+{
+    res.sendFile(path.join(__dirname + '/Trainer_has_Training_Session.html'));      
+}); 
+
+app.get('/Training_Sessions', function(req, res)                
+{
+    res.sendFile(path.join(__dirname + '/Training_Sessions.html'));      
 }); 
 /*
     LISTENER
