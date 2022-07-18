5a7bdbd53cd60167daaabfb89b574672623b6136
Daniel Bracamontes <bracamod@flip2.engr.oregonstate.edu>
2022-07-18 16:57:59 -0700

    Configured to run in public_html

--- BADT_HTML/app.js
+++ BADT_HTML/app.js
@@ -14,54 +14,54 @@ app.use(express.urlencoded());
 // app.engine('handlebars', handlebars.engine)
 // app.set('view engine', 'handlebars')
 //app.use(express.static(path.join(__dirname + '/public')));
-app.set('port', 3000);              // Set a port number at the top so it's easy to change in the future
+app.set('port', 52521);              // Set a port number at the top so it's easy to change in the future
 
 /*
     ROUTES
 */
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/Index.html'));      
-}); 
+app.get('/Index', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Index.html'));
+});
 
-app.get('/Customers', function(req, res) {
-    res.sendFile(path.join(__dirname + '/Customers.html'));      
-});                                         
+app.get('/Customers', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Customers.html'));
+});
 
-app.get('/Dogs', function(req, res) {
-    res.sendFile(path.join(__dirname + '/Dogs.html'));      
-});       
+app.get('/Dogs', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Dogs.html'));
+});
 
-app.get('/Dog_has_Training_Session', function(req, res) {
-    res.sendFile(path.join(__dirname + '/Dog_has_Training_Session.html'));      
-}); 
+app.get('/Dog_has_Training_Session', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Dog_has_Training_Session.html'));
+});
 
-app.get('/Packages', function(req, res) {
-    res.sendFile(path.join(__dirname + '/Packages.html'));      
-}); 
+app.get('/Packages', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Packages.html'));
+});
 
-app.get('/Purchases', function(req, res) {
-    res.sendFile(path.join(__dirname + '/Purchases.html'));      
-}); 
+app.get('/Purchases', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Purchases.html'));
+});
 
-app.get('/Session_Types', function(req, res) {
-    res.sendFile(path.join(__dirname + '/Session_Types.html'));      
-}); 
+app.get('/Session_Types', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Session_Types.html'));
+});
 
-app.get('/Trainers', function(req, res) {
-    res.sendFile(path.join(__dirname + '/Trainers.html'));      
-}); 
+app.get('/Trainers', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Trainers.html'));
+});
 
-app.get('/Trainer_has_Training_Session', function(req, res) {
-    res.sendFile(path.join(__dirname + '/Trainer_has_Training_Session.html'));      
-}); 
+app.get('/Trainer_has_Training_Session', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Trainer_has_Training_Session.html'));
+});
 
-app.get('/Training_Sessions', function(req, res) {
-    res.sendFile(path.join(__dirname + '/Training_Sessions.html'));      
-}); 
+app.get('/Training_Sessions', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Training_Sessions.html'));
+});
 /*
     LISTENER
 */
-app.listen(app.get("port"), function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+app.listen(app.get("port"), function () {            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
     console.log(`Express started on http://localhost:${app.get('port')}; press Ctrl-C to terminate.`)
 });
